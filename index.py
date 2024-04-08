a27c70b835c3968f5794dad42c2d0272aecb64e7
Sadra Sahraei <sadrasahraei04@gmail.com>
2024-04-08 13:26:42 -0700

    update

--- index.py
+++ index.py
@@ -1,15 +1,6 @@
 
-
-
-
-
-
-
 from tkinter import *
-
 import random
-
-
 
 GAME_WIDTH = 1000
 GAME_HEIGHT = 550
@@ -20,20 +11,10 @@ SNAKE_COLOR ="green"
 FOOD_COLOR ="green"
 BACKGROUND_COLOR = "#000000"
 
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
-
 
 class Snake:
 
-    
     def __init__(self):
         self.body_size = BODY_PARTS
         self.coordinates = []
@@ -46,20 +27,9 @@ class Snake:
             square = canvas.create_rectangle(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(square)
 
-
-    
-
-   
 #-----------------------------------------------------------------------------------------
 
-
-
-
-
-
-
 class Food:
-    
     
     def __init__(self):
         x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
@@ -67,19 +37,7 @@ class Food:
         self.coordinates = [x, y]
         canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
-
-    
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 def next_turn(snake, food):
     
@@ -115,15 +73,7 @@ def next_turn(snake, food):
     else:
         window.after(SPEED, next_turn, snake, food)
     
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
 
 def change_direction(new_direction):
     
@@ -144,21 +94,8 @@ def change_direction(new_direction):
     elif new_direction == "down":
         if direction != "up":
             direction = new_direction
-    
 
-
-
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 def check_collisions(snake):
     
@@ -174,38 +111,13 @@ def check_collisions(snake):
         return True
     return False
 
-
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
 
 def game_over():
-    
     canvas.delete(ALL)
-    
     canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=("consolas", 70), text="GAME OVER", fill="red", tag="gameover")
 
-
-
-
-
-    
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 window = Tk()
 window.title("Snake game")
@@ -216,10 +128,8 @@ direction = 'down'
 
 label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
 label.pack()
-
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
-
 
 window.update()
 window_width = window.winfo_width()
@@ -227,31 +137,18 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
-
-
-
 window.bind("<Left>", lambda event:change_direction("left"))
 window.bind("<Right>", lambda event:change_direction("right"))
 window.bind("<Up>", lambda event:change_direction("up"))
 window.bind("<Down>", lambda event:change_direction("down"))
 
-
-
-
 snake = Snake()
 food = Food()
 next_turn(snake, food)
     
-
-   
 #-----------------------------------------------------------------------------------------
-
-
-
 window.mainloop()
